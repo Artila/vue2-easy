68b3766220daf60a51fb11d91f978b4eb9c26940
Artila <529276960@qq.com>
2018-02-18 01:48:59 +0800

    router

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   // 按需加载组件
+    //   component: r => require.ensure([], () => r(require('@/components/HelloWorld')), 'HelloWorld')
+    // }
+    {
+      path: '/',
+      name: 'Home',
+      // 按需加载组件
+      component: r => require.ensure([], () => r(require('@/page/home')), 'Home')
+    },
+    {
+      path: '/item',
+      name: 'Item',
+      component: r => require.ensure([], () => r(require('@/page/item')), 'Item')
+    }
+  ]
+})
