70784bb87c0edd7b0d5f3a16c526a2f93af1afe7
Artila <529276960@qq.com>
2018-03-28 00:01:57 +0800

    答题

--- src/page/item/index.vue
+++ src/page/item/index.vue
@@ -1,35 +1,94 @@
 <template>
   <div class="home_container">
     <header class="top_tips">
-      <span class="num_tip">0</span>
-      <span class="num_tip">题目</span>
+      <span class="num_tip">题目{{itemNum}}</span>
     </header>
     <div>
       <div class="item_back item_container_style">
         <div class="item_list_container">
-          <header class="item_title"></header>
+          <header class="item_title">
+            {{ itemDetail[itemNum-1].topic_name }}
+          </header>
           <ul>
-            <li class="item_list">
-              <span class="option_style"></span>
-              <span class="option_detail"></span>
+            <li class="item_list" v-for="(item, index) in itemDetail[itemNum - 1].topic_answer" :key="index" @click="choosed(index, item.topic_answer_id)">
+              <span class="option_style" :class="{'has_choosed': choosedNum == index}">
+                {{chooseType(index)}}
+              </span>
+              <span class="option_detail">
+                {{item.answer_name}}
+              </span>
             </li>
           </ul>
         </div>
       </div>
-      <span class="next_item button_style"></span>
-      <span class="submit_item button_style"></span>
+      <span class="next_item button_style" v-if="itemNum < itemDetail.length" @click="nextItem"></span>
+      <span class="submit_item button_style" v-else @click="submitAnswer"></span>
     </div>
   </div>
 </template>
 
 <script>
+  import { mapState, mapActions } from 'vuex';
+
   export default {
     data () {
       return {
-      }
+        itemId: null,      // 题目ID
+        choosedNum: null,  // 选中答案索引
+        choosedId: null,   // 选中答案id
+      };
     },
-    components: {},
-    methods: {}
+    computed: mapState([
+      "itemNum",     // 第几题
+      "level",       // 第几周
+      "itemDetail",  // 题目详情
+      "timer",       // 计时器
+    ]),
+    methods: {
+      ...mapActions(["addNum", "initializeData"]),
+      // 点击下一题
+      nextItem() {
+        if (this.choosedNum !== null) {
+          this.choosedNum = null;
+          // 保存答案，题目索引加1，跳到下一题
+          this.addNum(this.choosedId);
+        } else {
+          alert("你还没有答题！");
+        }
+      },
+      // 索引0-3对应答案A-D
+      chooseType: type=> {
+        switch (type) {
+          case 0:
+            return 'A';
+          case 1:
+            return 'B';
+          case 2:
+            return 'C';
+          case 3:
+            return 'D';
+        }
+      },
+      // 选中的答案信息
+      choosed (type, id) {
+        this.choosedNum = type;
+        this.choosedId = id;
+      },
+      // 到达最后一题，交卷，清空定时器，跳转分数页面
+      submitAnswer() {
+        if (this.choosedNum !== null) {
+          this.addNum(this.choosedId);
+          clearInterval(this.timer);
+          this.$router.push("score");
+        } else {
+          alert('你还没有答题。');
+        }
+      },
+    },
+    created () {
+      this.initializeData();
+      // document.body.style.backgroundImage = "url('~assets/images/1-1.jpg')";
+    },
   }
 
 </script>
